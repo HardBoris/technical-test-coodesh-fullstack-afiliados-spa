bff2f6f78240c1bbe9f79e01fa0234e020f4ee1f
HardBoris <bgaibor03@gmail.com>
2023-07-23 12:01:20 -0400

    feat: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,8 +1,59 @@
+import "./login.style.css";
+import { Button } from "../../components/Button";
+import { Formulario } from "../../components/Form";
+import { Input } from "../../components/Input";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { SignInInfo, useAuth } from "../../context/UserContext";
+import { useForm } from "react-hook-form";
+
+const loginSchema = yup.object().shape({
+  userName: yup.string().required(),
+  userPassword: yup.string().required(),
+});
+
 export const Login = () => {
+  const { signIn } = useAuth();
+
+  const {
+    formState: { errors },
+    register,
+    handleSubmit,
+  } = useForm<SignInInfo>({ resolver: yupResolver(loginSchema) });
+
+  const sender = (info: SignInInfo) => {
+    signIn(info);
+  };
+
   return (
     <>
-      <div>
-        <h1>Login</h1>
+      <div className="login">
+        <div className="login-sheath">
+          <Formulario onSubmit={handleSubmit(sender)}>
+            <div className="input-form">
+              <h4>Login</h4>
+            </div>
+            <Input
+              type="text"
+              name="userName"
+              placeholder="Usuário"
+              register={register}
+              error={errors.userName?.message}
+            />
+            <Input
+              type="password"
+              name="userPassword"
+              placeholder="Senha"
+              register={register}
+              error={errors.userPassword?.message}
+            />
+            <div className="input-form">
+              <Button type="submit" variant="neutro yes">
+                Entrar
+              </Button>
+            </div>
+          </Formulario>
+        </div>
       </div>
     </>
   );
