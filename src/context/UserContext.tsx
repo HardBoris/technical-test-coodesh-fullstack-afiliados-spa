7f2e19c51733db5f0e7a72b5347b1f2a8d291683
HardBoris <bgaibor03@gmail.com>
2023-07-23 09:45:58 -0400

    refactor: user context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,42 +1,31 @@
 import { createContext, ReactNode, useContext, useState } from "react";
-import { useNavigate } from "react-router-dom";
-// import { toast } from "react-toastify";
-
 import { localApi as api } from "../services/api";
-// import { api } from "../services/api";
 
 interface UserProviderProps {
   children: ReactNode;
 }
 
-interface User {
+export interface User {
   userId: string;
   userName: string;
-  email: string;
+  userPassword?: string;
 }
 
 interface AuthState {
   token: string;
   user: User;
-  company: string;
 }
 
-interface SignInCredentials {
+export interface SignInInfo {
   userName: string;
-  userPassword: string;
-  companyCode: string;
+  userPassword?: string;
 }
 
 interface UserContextData {
   user: User;
   token: string;
-  company: string;
-  signIn: (credentials: SignInCredentials) => Promise<void>;
+  signIn: (credentials: SignInInfo) => Promise<void>;
   signOut: () => void;
-  signUp: (info: SignInCredentials) => void;
-  // mensaje: string;
-  // email: string;
-  // status: number;
 }
 
 const UserContext = createContext<UserContextData>({} as UserContextData);
@@ -52,90 +41,34 @@ const useAuth = () => {
 };
 
 const UserProvider = ({ children }: UserProviderProps) => {
-  const history = useNavigate();
-  const [logedUser, setLogedUser] = useState("");
-  // const [messageError, setMessageError] = useState("");
-  // const [status, setStatus] = useState(0);
-
   const [data, setData] = useState<AuthState>(() => {
-    const token = localStorage.getItem("@Aventura:token");
-    const user = localStorage.getItem("@Aventura:user");
-    const company = localStorage.getItem("@Aventura:company");
+    const token = localStorage.getItem("@Coodesh:token");
+    const user = localStorage.getItem("@Coodesh:user");
 
-    if (token && user && company) {
-      return { token, user: JSON.parse(user), company };
+    if (token && user) {
+      return { token, user: JSON.parse(user) };
     }
 
     return {} as AuthState;
   });
 
-  const signIn = async ({
-    userName,
-    userPassword,
-    companyCode,
-  }: SignInCredentials) => {
-    // const aviso = toast.loading("Por Favor espere...");
+  const signIn = async ({ userName, userPassword }: SignInInfo) => {
     await api
-      .post("/login", { userName, userPassword, companyCode })
+      .post("/login", { userName, userPassword })
       .then((response) => {
-        const { user, token, company } = response.data;
-        localStorage.setItem("@Aventura:token", token);
-        localStorage.setItem("@Aventura:user", JSON.stringify(user));
-        localStorage.setItem("@Aventura:company", company);
-        setData({ user, token, company });
-        history(`/${company}`);
-        /* toast.update(aviso, {
-          render: "Bem-Vindo a Oikos!",
-          type: "success",
-          isLoading: false,
-          autoClose: 3000,
-        }); */
+        const { user, token } = response.data;
+        localStorage.setItem("@Coodesh:token", token);
+        localStorage.setItem("@Coodesh:user", JSON.stringify(user));
+        setData({ user, token });
       })
       .catch((error) => {
         console.log(error);
-        /* toast.update(aviso, {
-          render: error.response.data.message,
-          type: "error",
-          isLoading: false,
-          autoClose: 3000,
-        }); */
-      });
-  };
-
-  const signUp = async ({ userName, userPassword }: SignInCredentials) => {
-    // const aviso = toast.loading("Por Favor espere...");
-    await api
-      .post("/:companyId/users/register", {
-        userName,
-        userPassword,
-      })
-      .then((response) => {
-        console.log(response.data);
-        const { usuario } = response.data;
-        setLogedUser(usuario);
-        /* toast.update(aviso, {
-          render: "Novo usuário cadastrado!",
-          type: "success",
-          isLoading: false,
-          autoClose: 3000,
-        }); */
-      })
-      .then(() => history("/login"))
-      .catch((error) => {
-        console.log(error);
-        /* toast.update(aviso, {
-          render: error.response.data.message,
-          type: "error",
-          isLoading: false,
-          autoClose: 3000,
-        }); */
       });
   };
 
   const signOut = () => {
-    localStorage.removeItem("@Aventura:token");
-    localStorage.removeItem("@Aventura:user");
-    localStorage.removeItem("@Aventura:company");
+    localStorage.removeItem("@Coodesh:token");
+    localStorage.removeItem("@Coodesh:user");
 
     setData({} as AuthState);
   };
@@ -145,13 +78,8 @@ const UserProvider = ({ children }: UserProviderProps) => {
       value={{
         token: data.token,
         user: data.user,
-        company: data.company,
         signIn,
         signOut,
-        signUp,
-        // mensaje,
-        // email,
-        // status,
       }}
     >
       {children}
