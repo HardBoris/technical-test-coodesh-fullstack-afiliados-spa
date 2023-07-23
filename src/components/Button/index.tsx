76d1fe77a15edcb485351bed927028d178edccde
HardBoris <bgaibor03@gmail.com>
2023-07-23 12:00:37 -0400

    feat: button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import { ButtonHTMLAttributes, ReactNode } from "react";
+import "./button.style.css";
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: ReactNode;
+  variant?: string;
+}
+
+export const Button = ({
+  children,
+  variant = "neutro",
+  ...rest
+}: ButtonProps) => (
+  <div className="btn">
+    <button {...rest} className={variant}>
+      {children}
+    </button>
+  </div>
+);
