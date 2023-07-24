eb19fc7cfcfabed51f40c6f588101692ce40ee9d
HardBoris <bgaibor03@gmail.com>
2023-07-24 16:27:15 -0400

    feat: save and get movements

--- src/context/MovementContext.tsx
+++ src/context/MovementContext.tsx
@@ -1,4 +1,6 @@
 import { ReactNode, createContext, useContext, useState } from "react";
+import { localApi as api } from "../services/api";
+import { useAuth } from "./UserContext";
 
 interface MovementProviderProps {
   children: ReactNode;
@@ -23,6 +25,9 @@ export interface MovementInfo {
 
 interface MovementContextData {
   movement: Movement;
+  moveList: Movement[];
+  saveMovement: (info: MovementInfo) => void;
+  movementsList: () => void;
 }
 
 export const MovementContext = createContext<MovementContextData>(
@@ -32,9 +37,38 @@ export const MovementContext = createContext<MovementContextData>(
 const useMovement = () => useContext(MovementContext);
 
 const MovementProvider = ({ children }: MovementProviderProps) => {
+  const { token } = useAuth();
   const [movement, setMovement] = useState<Movement>({} as Movement);
+  const [moveList, setMoveList] = useState<Movement[]>([]);
+
+  const saveMovement = async ({
+    type,
+    date,
+    product,
+    price,
+    seller,
+  }: MovementInfo) => {
+    await api
+      .post(
+        "/movements/register",
+        { type, date, product, price, seller },
+        { headers: { authorization: `Bearer ${token}` } }
+      )
+      .then((response) => setMovement(response.data))
+      .catch((error) => console.log(error));
+  };
+
+  const movementsList = async () => {
+    await api
+      .get("/movements", { headers: { authorization: `Bearer ${token}` } })
+      .then((response) => setMoveList(response.data))
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <MovementContext.Provider value={{ movement }}>
+    <MovementContext.Provider
+      value={{ movement, moveList, saveMovement, movementsList }}
+    >
       {children}
     </MovementContext.Provider>
   );
